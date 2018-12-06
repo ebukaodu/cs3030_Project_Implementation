087b49dc1b2735627f6171b3f367449449b56d0b
ebukaodu <chukas4christ@gmail.com>
2018-12-06 01:42:05 -0700

    Added another method to the project

--- webQuerying.py
+++ webQuerying.py
@@ -1,18 +1,22 @@
-from time import gmtime, strftime
+#!/usr/bin/python3
 import time
+import urllib
+import pygeoip
 import configparser
-import geocoder
+import json
+from contextlib import closing
 import urllib.request
-import requests, json 
 import sys
-import os
+import requests
+import socket
+import re
 
 def finddistance():
   noError = True
   while noError:
     try:
       # Take source as input 
-      source = input("Enter your locatio: ") 
+      source = input("Enter your location: ") 
         
       # Take destination as input 
       dest = input("Enter your destination: ") 
@@ -36,12 +40,38 @@ def finddistance():
       print("It will take " + time.strftime("%H hours %M minutes %S seconds", time.gmtime(duration)))
       noError = False
     except:
-      print("Check your spelling")
+      print("Check your spelling \n Only enter the name of the cities.")
       continue
+
+
+
+def getIpLocation(url):
+  url = url.split("//")[-1].split("/")[0].split('?')[0]
+  url = socket.gethostbyname(url)
+  url = 'http://ip-api.com/json/' + url
+  
+  req = requests.get(url)
+  out =  req.json()
+  country = out['country']
+  region = out['regionName']
+  city = out['city']
+  zipcod = out['zip']
+
+  lat = out['lat']
+  lon = out['lon']
+  status = out['status']
+  timezone = out['timezone']
+  ip = out["query"]
+
+  print (country,region,city,zipcod)
+  print(lat,lon,status,timezone,ip)
+
+  
 
 def main():
   finddistance()
-
+  url = "https://www.weber.edu/"
+  getIpLocation(url)
 
 
 main()
